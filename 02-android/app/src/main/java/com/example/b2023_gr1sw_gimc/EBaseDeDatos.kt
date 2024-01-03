1e21303c594be8ce59221301da2470ce62058051
Glender Miranda <glender.miranda@epn.edu.ec>
2024-01-03 12:48:50 -0500

    Clase 2 de enero 2024

---
+++ 02-android/app/src/main/java/com/example/b2023_gr1sw_gimc/EBaseDeDatos.kt
@@ -0,0 +1,9 @@
+package com.example.b2023_gr1sw_gimc
+
+import com.example.b2023_gr1sw_gimc.ui.theme.ESqliteHelperEntrenador
+
+class EBaseDeDatos {
+    companion object{
+        var tablaEntrenador: ESqliteHelperEntrenador? = null
+    }
+}
--- 02-android/app/src/main/java/com/example/b2023_gr1sw_gimc/MainActivity.kt
+++ 02-android/app/src/main/java/com/example/b2023_gr1sw_gimc/MainActivity.kt
@@ -1,15 +1,81 @@
 package com.example.b2023_gr1sw_gimc
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.example.b2023_gr1sw_gimc.ui.theme.ESqliteHelperEntrenador
+import com.google.android.material.snackbar.Snackbar
 
 
 class MainActivity : AppCompatActivity() {
+
+    val callbackContenidoIntentExplicito =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+
+        ){
+            result ->
+            if (result.resultCode == Activity.RESULT_OK){
+                if(result.data!=null){
+                    //Logica Negocio
+                    val data = result.data
+                    mostrarSnackbar(
+                        "${data?.getStringExtra("nombreModificado")}"
+                    )
+                }
+            }
+        }
+
+
+    fun mostrarSnackbar(texto:String){
+        Snackbar
+            .make(
+                findViewById(R.id.id_layout_main), //View
+            texto, //texto
+            Snackbar.LENGTH_LONG //Tiempo
+            )
+            .show()
+    }
+
+    val callbackIntentPickUri =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ){
+                result ->
+            if (result.resultCode == Activity.RESULT_OK){
+                if(result.data != null){
+                    if(result.data!!.data != null){
+                        val uri: Uri = result.data!!.data!!
+                        val cursor = contentResolver.query(
+                            uri,null,null,null,null,null
+                        )
+                        cursor?.moveToFirst()
+                        val indiceTelefono = cursor?.getColumnIndex(
+                            ContactsContract.CommonDataKinds.Phone.NUMBER
+                        )
+                        val telefono = cursor?.getString(indiceTelefono!!)
+                        cursor?.close()
+                        mostrarSnackbar("Telefono ${telefono}")
+                    }
+                }
+            }
+
+        }
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Base de datos sqlite
+        EBaseDeDatos.tablaEntrenador = ESqliteHelperEntrenador(this)
+
         val botonCicloVida = findViewById<Button>(R.id.btn_ciclo_vida)
         botonCicloVida
             .setOnClickListener{
@@ -20,6 +86,46 @@ class MainActivity : AppCompatActivity() {
             .setOnClickListener{
                 irActividad(BListView::class.java)
             }
+
+        val botonIntentImplicto = findViewById<Button>(
+            R.id.btn_ir_intent_implicito)
+        botonIntentImplicto
+            .setOnClickListener{
+                val intentConRespuesta = Intent(
+                    Intent.ACTION_PICK,
+                    ContactsContract.CommonDataKinds.Phone.CONTENT_URI
+                )
+                callbackIntentPickUri.launch(intentConRespuesta)
+            }
+
+        val botonIntentExplicito = findViewById<Button>(
+            R.id.btn_ir_intent_explicito
+        )
+        botonIntentExplicito
+            .setOnClickListener{
+                abrirActividadConParametros(
+                    CIntentExplicitoParametros::class.java
+                )
+            }
+        val botonSqlite = findViewById<Button>(R.id.btn_sqlite)
+        botonSqlite
+            .setOnClickListener {
+                irActividad(ECrudEntrenador::class.java)
+            }
+
+
+    }//Termina onCreate
+
+    fun abrirActividadConParametros(
+        clase: Class<*>
+    ){
+        val intentExplicito = Intent(this, clase)
+        //Enviar parametros (solamente variables primitivas)
+        intentExplicito.putExtra("nombre","Glender")
+        intentExplicito.putExtra("apellido","Miranda")
+        intentExplicito.putExtra("edad",22)
+
+        callbackContenidoIntentExplicito.launch(intentExplicito)
     }
 
     fun irActividad(
